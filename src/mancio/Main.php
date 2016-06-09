c6811ca7c2a893ec3f822f5c1e91ac99b58fa5cd
Mattia990 <dyfg@hotmail.it>
2016-06-09 13:42:14 +0200

    Added /config to configure first start of server

--- src/mancio/Main.php
+++ src/mancio/Main.php
@@ -2,6 +2,7 @@
 
 namespace mancio;
 
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -12,12 +13,14 @@ use pocketmine\utils\TextFormat;
 use pocketmine\level\Location;
 use pocketmine\Player;
 
+
 class Main extends PluginBase implements Listener {
 	
 	public static $login=true;
 	public static $tentativi=0;
 	public static $exLoc;
 	public static $name="";
+	public static $first;
 	
 	public function onEnable() {
 		$this->getLogger()->info("Plugin abilitato!");
@@ -38,6 +41,11 @@ class Main extends PluginBase implements Listener {
 		Main::$exLoc=$player->getLocation();
 		Main::$login=false;
 		Main::$name=$player->getName();
+		Main::$first=$this->getConfig()->get("First Start");
+		
+		if(Main::$first) {
+			$player->sendMessage(TextFormat::RED . "Se sei OP usa il comando /config <password> <x> <y> <z> per configurare il plugin!");
+		}
 	}
 	
 	public function onPlayerMove(PlayerMoveEvent $event) {
@@ -45,7 +53,7 @@ class Main extends PluginBase implements Listener {
 		
 		if(strcasecmp($player->getName(), Main::$name) == 0) {
 			if(!Main::$login) {
-				$loc=new Location(142,71,914);
+				$loc=new Location($this->getConfig()->get("Spawn Location"));
 				$player->teleport($loc);
 			}
 		}
@@ -54,56 +62,87 @@ class Main extends PluginBase implements Listener {
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
 		$player=$this->getServer()->getPlayer($sender->getName());
 		
-		if(strcasecmp($cmd->getName(), "login") == 0 && $sender instanceof Player) {
-			if(!Main::$login) {
-				if($args[0] != $this->getConfig()->get("Password")) {
-					Main::$tentativi++;
-					
-					if(Main::$tentativi== 3) {
-						$player->kick(TextFormat::RED . "Hai sbagliato la password per 3 volte consecutive");
+		if(!Main::$first) {
+			if(strcasecmp($cmd->getName(), "login") == 0 && $sender instanceof Player) {
+				if(!Main::$login) {
+					if($args[0] != $this->getConfig()->get("Password")) {
+						Main::$tentativi++;
+						
+						if(Main::$tentativi== 3) {
+							$player->kick(TextFormat::RED . "Hai sbagliato la password per 3 volte consecutive");
+						}
+						$player->sendMessage(TextFormat::YELLOW . "Hai sbagliato la passowrd!, Riprova");
+						Main::$login=false;
+						
+					} else {
+						Main::$login=true;
+						$player->sendMessage(TextFormat::GRAY . "Password corretta, ora puoi giocare nel server");
+						$player->teleport(Main::$exLoc);
 					}
-					$player->sendMessage(TextFormat::YELLOW . "Hai sbagliato la passowrd!, Riprova");
-					Main::$login=false;
-					
 				} else {
-					Main::$login=true;
-					$player->sendMessage(TextFormat::GRAY . "Password corretta, ora puoi giocare nel server");
-					$player->teleport(Main::$exLoc);
+					$player->sendMessage(TextFormat::RED . "Login gia' eseguito!");
 				}
-			} else {
-				$player->sendMessage(TextFormat::RED . "Login gia' eseguito!");
+				return true;
 			}
-			return true;
-		}
-		
-		if(strcasecmp($cmd->getName(), "cambiapass") == 0 && $sender instanceof Player) {
-			if($player->isOp()) {
+			
+			if(strcasecmp($cmd->getName(), "cambiapass") == 0 && $sender instanceof Player) {
+				if($player->isOp()) {
+					$this->getConfig()->set("Password", $args[0]);
+					$this->saveConfig();
+					$player->sendMessage(TextFormat::GREEN . "La password e' stata cambiata con successo!");
+				} else {
+					$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
+				}
+				return true;
+			}
+			
+			if(strcasecmp($cmd->getName(), "mostrapass") == 0 && $sender instanceof Player) {
+				if($player->isOp()) {
+					$password=$this->getConfig()->get("Password");
+					$player->sendMessage(TextFormat::BOLD . "La password attuale e' " . $password);
+				} else {
+					$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
+				}
+				return true;
+			}
+			
+			/*if(strcasecmp($cmd->getName(), "resetpass") == 0 && $sender instanceof Player) {
+				if($player->isOp()) {
+					$this->getConfig()->set("Password", $this->getConfig()->get("Password"));
+					$this->saveConfig();
+					$player->sendMessage(TextFormat::GREEN . "La password e' stata ripristinata con successo!");
+				} else {
+					$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
+				}
+				return true;
+			}*/
+			
+			if(strcasecmp($cmd->getName(), "config") == 0) {
 				$this->getConfig()->set("Password", $args[0]);
+				$this->getConfig()->set("First Start", false);
+				$this->getConfig()->set("X", $args[1]);
+				$this->getConfig()->set("Y", $args[2]);
+				$this->getConfig()->set("Z", $args[3]);
+				$this->getConfig()->set("Spawn Location", $args[1] . ", " . $args[2] . ", " . $args[3]);
+			
 				$this->saveConfig();
-				$player->sendMessage(TextFormat::GREEN . "La password e' stata cambiata con successo!");
-			} else {
-				$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
+				
+				$player->sendMessage("Plugin configurato correttamente!");
 			}
 			return true;
-		}
-		
-		if(strcasecmp($cmd->getName(), "mostrapass") == 0 && $sender instanceof Player) {
-			if($player->isOp()) {
-				$password=$this->getConfig()->get("Password");
-				$player->sendMessage(TextFormat::BOLD . "La password attuale e' " . $password);
-			} else {
-				$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
-			}
-			return true;
-		}
-		
-		if(strcasecmp($cmd->getName(), "resetpass") == 0 && $sender instanceof Player) {
-			if($player->isOp()) {
-				$this->getConfig()->set("Password", "gatto986");
+			
+		} else {
+			if(strcasecmp($cmd->getName(), "config") == 0) {
+				$this->getConfig()->set("Password", $args[0]);
+				$this->getConfig()->set("First Start", false);
+				$this->getConfig()->set("X", $args[1]);
+				$this->getConfig()->set("Y", $args[2]);
+				$this->getConfig()->set("Z", $args[3]);
+				$this->getConfig()->set("Spawn Location", $args[1] . ", " . $args[2] . ", " . $args[3]);
+				
 				$this->saveConfig();
-				$player->sendMessage(TextFormat::GREEN . "La password e' stata ripristinata con successo!");
-			} else {
-				$player->sendMessage(TextFormat::RED . "Non puoi utilizzare questo comando, non sei OP!");
+				
+				$player->sendMessage("Plugin configurato correttamente!");
 			}
 			return true;
 		}
